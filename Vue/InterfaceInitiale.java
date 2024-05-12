491eee4558fc4a3d8ad11a629e62ad98980f3ad1
madhbouh Yassine <147738834+Yassine-Md@users.noreply.github.com>
2024-05-12 15:07:49 +0200

    Delete Vue directory

--- Vue/InterfaceInitiale.java
+++
@@ -1,9 +0,0 @@
-package Vue;
-
-import javax.swing.*;
-
-public class InterfaceInitiale extends JFrame {
-    public static void demarrer() {
-        SwingUtilities.invokeLater(InterfaceInitiale::new);
-    }
-}
