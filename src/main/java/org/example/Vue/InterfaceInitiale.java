c0ed1c9bffac3b2b859d91ad7c8101b8c50e9ce1
Mohamed Amine El Ouechrine <150486003+amine-elouechrine@users.noreply.github.com>
2024-06-04 22:07:13 +0200

    ameliorations

--- src/main/java/org/example/Vue/InterfaceInitiale.java
+++ src/main/java/org/example/Vue/InterfaceInitiale.java
@@ -120,30 +120,109 @@ public class InterfaceInitiale extends JFrame implements Runnable {
         this.setVisible(false);
     }
     private void showRules() {
+        String rules = """
+        Set up
+        Shuffle all cards and place them in a face-down deck in the middle of the table.
+        Deal each player 13 cards. Each player takes their cards into their hand without showing these to the other player.
 
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Image Display");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(500, 640);
-            Image backgroundImage = null;
-            // Charge l'image depuis un fichier (remplacez le chemin par le vôtre)
-            try {
-                java.net.URL imgURL = getClass().getResource("/f8-claim-rulebook.png");
-                if (imgURL != null) {
-                    backgroundImage = new ImageIcon(imgURL).getImage();
-                    JLabel label = new JLabel(new ImageIcon(backgroundImage));
-                    frame.add(label);
-                } else {
-                    throw new IOException("Image not found");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.err.println("Erreur de chargement de l'image de fond");
-            }
+        Game overview
+        The game is played in two distinct phases. In Phase One, each player gets a hand of cards that they will use to recruit
+        followers. In Phase Two, they will use the followers they gained in Phase One to compete and win over the five factions
+        of the realms. At the end of the game the player who has the majority of followers of a faction wins that faction’s vote. The
+        player who wins the vote of at least three factions wins the game!
 
-        });
+        Phase One: Recruit followers
+        This phase consists of thirteen tricks, one for each card in each player’s hand. The Leader for each trick is the player who won the last
+        trick (or in the case of the first trick the youngest player).
+        Each trick is played by following these three steps:
+        1. Reveal one card
+           Flip the top card from the deck in the middle of the table and place it between both players. The players will be
+           competing for this card.
+        2. Play cards
+           1. The Leader plays any single card from his hand.
+              Note: He can choose a card of any faction, this doesn’t have to be a card of the faction the players are competing for.
+           2. Then the other player plays any single card from their hand.
+              Important: If able, the other player must follow faction. Which means, if this player has a card in their hand of the
+              faction of the card just played then it must be played (exception: Doppelgänger). In other words, a player can only
+              play a card of a different faction if no card in their hand matches the faction of the one played by the Leader.
+        3. Collect cards
+           1. The player that played the highest valued card (0 is the lowest, 9 the highest) of the faction played by the Leader wins
+              the card in the middle. In case of a tie, the Leader wins the trick.
+              Note: If the second player played a card of another faction then the Leader automatically wins the trick, unless a
+              faction power (Doppelgänger or Knight) is in effect.
+           2. The winner places their won card face-down in their Follower deck. The loser takes a card from the top of the deck in the
+              middle and places it face-down in their Follower deck. This player may look at the card, but is not allowed to show it to the
+              other player.
+              Note: Cards in your Follower deck will be your hand for Phase Two.
+           3. If either player has played an Undead card, the winner of the trick takes them and places them face-up in their Score pile.
+              Important: A won Undead card from the middle of the table still goes to the winner’s Followers pile.
+           4. Discard all (remaining) cards played.
+              Important: Be sure to keep your Score pile face-up and your Follower deck face-down to keep them separated.
+        Continue until the deck is depleted and the players are out of cards in their hand.
+        Now the game moves onto Phase Two.
 
+        Phase Two: Gather supporters
+        Both players take the 13 Follower cards they gained during Phase One into their hand. Now, players will play another
+        13 tricks. However, instead of competing for particular cards in the middle, this time players compete for both cards
+        played each trick.
+        Each trick is played by following these two steps:
+        1. Play cards
+           1. The leader plays any single card from their hand.
+           2. Then the other player plays any single card from their hand.
+              Important: The rules regarding following faction are still in effect!
+        2. Collect cards
+           1. Determine the winner like in Phase One.
+           2. The winner of the trick places both played cards face-up in their Score pile, unless a faction power (Dwarves) is in effect.
+
+        After all players have played all cards from their hand, all factions are scored.
+
+        End of the game & Scoring
+        The players count how many cards of each faction they have in their Score pile. Whoever has the most cards of a faction
+        wins that faction’s vote. If there is a tie, whoever has the highest single card of that faction wins the vote. The player
+        who wins the vote of at least three factions wins the game!
+
+        Faction’s special powers
+        Each faction has a special power that effects play. They are as follows:
+
+        Goblins
+        No special power.
+
+        Knights
+        When played after a Goblin, it automatically beats a Goblin regardless of its value.
+        Important: The player must still follow faction, if able.
+
+        Undead
+        Played Undead cards are not discarded in Phase One like the other faction’s cards, but instead are added to
+        the trick winner’s Score Pile.
+
+        Dwarves
+        In Phase Two, the player losing a trick collects all Dwarves played during this trick and adds them to their
+        Score Pile. The winner will still collect any non-Dwarves cards that have been played.
+
+        Doppelgängers
+        This faction is considered wild. You may play a Doppelgänger instead of the asked suit, even if you’re able to
+        follow suit. When played second, it is considered to be the same faction of the first card and is considered to be
+        following faction.
+        Note: If the Leader plays a Doppelgänger, the other player still must follow faction by playing a Doppelgänger,
+        if able.
+        Important: A Doppelgänger does not take any special powers from the faction it follows. For instance, if
+        played in Phase One after an Undead it does not get taken by the winner, nor would it get taken in Phase Two
+        by the loser like a Dwarf would.
+        """;
+
+        JTextArea textArea = new JTextArea(20, 50);
+        textArea.setText(rules);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setCaretPosition(0); // Scroll to the top
+        textArea.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        JOptionPane.showMessageDialog(null, scrollPane, "Règles du jeu", JOptionPane.INFORMATION_MESSAGE);
     }
+
 
 
 
