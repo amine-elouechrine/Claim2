2f5aa74640950cadd0af693d36c28b74b7092b64
emilien <emilien.maillard.simon@gmail.com>
2024-06-04 21:42:33 +0200

    Ajout classe manquante

--- src/main/java/org/example/Vue/ComposantFinPartie.java
+++ src/main/java/org/example/Vue/ComposantFinPartie.java
@@ -5,6 +5,7 @@ import org.example.Patternes.Observateur;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
